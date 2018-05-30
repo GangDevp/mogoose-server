eab1dc816e4553dc829130e5969b957508a80634
yinzhigang <yinzhigang@kedacom.com>
2018-05-30 12:50:04 +0800

    重构文件结构目录

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+let app = express();
 //设置跨域访问
-var allowCrossDomain = function (req, res, next) {
+const allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8888');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -16,14 +15,11 @@ var allowCrossDomain = function (req, res, next) {
 };
 app.use(allowCrossDomain);
 
-// view engine setup
-var ejs = require('ejs');
+let ejs = require('ejs');
 app.engine('html', ejs.__express);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -31,23 +27,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-var commonRouters = require('./src/routes/common-routers');
+const commonRouters = require('./src/routes/common-routers');
 app.use('/mogo', commonRouters);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
     res.status(err.status || 500);
     res.render('error');
 });
--- src/database/connect-db.js
+++ src/database/connect-db.js
@@ -1,10 +1,10 @@
 /**
  * 数据库连接公用
  */
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.set('debug', true);
-var db = mongoose.connect('mongodb://127.0.0.1/test');
+let db = mongoose.connect('mongodb://127.0.0.1/test');
 
 db.connection.on("error", function (error) {
     console.log("数据库连接失败：" + error);
