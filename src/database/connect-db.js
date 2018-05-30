165c09fded448e4f1430e938449e369f72228497
gangdevp <gangdev@163.com>
2018-05-30 23:14:39 +0800

    创建切面通知

--- src/database/connect-db.js
+++ src/database/connect-db.js
@@ -1,18 +1,38 @@
 /**
  * 数据库连接公用
  */
-const Aspect = require('./../util/aspect');
-const mongoose = require("mongoose");
+const aop = require('./../util/aspect');
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set('debug', true);
 let db = mongoose.connect('mongodb://127.0.0.1/test');
 
-db.connection.on("error", function (error) {
-    console.log("数据库连接失败：" + error);
+let dbConnectSuccessed = function () {
+    aop.logger('数据库服务开启中...');
+};
+
+let dbConnectFailed = function (error) {
+    aop.logger('数据库服务开启中...');
+    aop.logger('数据库服务开启失败：' + error);
+};
+
+
+dbConnectSuccessed = dbConnectSuccessed.before(function () {
+    console.log('数据库连接地址：mongodb://127.0.0.1/test');
+}).after(function () {
+    console.log('数据连接成功！');
 });
 
-db.connection.on("open", function () {
-    Aspect.getAspect()
-    Aspect.setAspect()
-    console.log("数据库连接成功!");
+dbConnectFailed = dbConnectFailed.before(function () {
+    console.log('数据库连接地址：mongodb://127.0.0.1/test');
+}).after(function () {
+    console.log('数据连接失败！');
+});
+
+db.connection.on('error', function (error) {
+    dbConnectFailed(error);
+});
+
+db.connection.on('open', function () {
+    dbConnectSuccessed();
 });
--- src/util/aspect.js
+++ src/util/aspect.js
@@ -1,11 +1,35 @@
-const Aspect = {
-    getAspect: () => {
-        console.log('get aspect');
-    },
-    setAspect: () => {
-        console.log('set aspect');
+const AOP = {
+    logger: (content, callback) => {
+        console.log(content);
+        if (callback) {
+            callback();
+        }
     }
 };
 
-exports['getAspect'] = Aspect.getAspect;
-exports['setAspect'] = Aspect.setAspect;
+
+Function.prototype.before = function (beforefn) {
+    var self = this;
+    return function () {
+        AOP.logger('###前置通知开始');
+        beforefn.apply(this, arguments);
+        AOP.logger('###前置通知结束');
+        return self.apply(this, arguments);
+    };
+};
+
+Function.prototype.after = function (afterfn) {
+    var self = this;
+    return function () {
+        var ret = self.apply(this, arguments);
+        AOP.logger('###后置通知开始');
+        afterfn.apply(this, arguments);
+        AOP.logger('###后置通知结束');
+        return ret;
+    };
+};
+
+
+
+
+exports['logger'] = AOP.logger;
